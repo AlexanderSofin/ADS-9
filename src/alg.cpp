9eae533312500303974628c5d40de0e29158c41c
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-05-21 22:47:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,25 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include <string>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  {
-    // поместите сюда свой код
-    BST<string> tree;
-    ifstream file(filename);
-    if (!file)
-    {
-        throw string("file not found!");
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::string("file not found!");
     }
-    string word = "";
-    while (!file.eof())
-    {
+    std::string word = "";
+    while (!file.eof()) {
         char chr = file.get();
         int ascii = chr;
-        if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122))
-        {
+        if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) {
             word += tolower(ascii);
-        }
-        else
-        {
-            if (word != "")
-            {
+        } else {
+            if (word != "") {
                 tree.add(word);
                 word = "";
             }
@@ -34,5 +27,4 @@ BST<std::string> makeTree(const char* filename) {
     }
     file.close();
     return tree;
-}
 }
